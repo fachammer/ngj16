36fa10f25f1b457372c0efe84161cc62b1ac9541
Fabian Achammer <fabian.achammer@gmail.com>
2016-04-09 15:16:06 +0200

    added world edges

--- Assets/Scripts/WrapAround.cs
+++ Assets/Scripts/WrapAround.cs
@@ -1,50 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class WrapAround : MonoBehaviour {
+public class WrapAround : MonoBehaviour
+{
 
     public float TeleportationMargin;
 
     private GameObject _destination;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
 
-        if(this.gameObject.name == "LeftEdge")
+        if (this.gameObject.name == "LeftEdge")
             _destination = GameObject.Find("RightEdge");
-        else if(this.gameObject.name == "RightEdge")
+        else if (this.gameObject.name == "RightEdge")
             _destination = GameObject.Find("LeftEdge");
-        else if(this.gameObject.name == "TopEdge")
+        else if (this.gameObject.name == "TopEdge")
             _destination = GameObject.Find("BottomEdge");
-        else if(this.gameObject.name == "BottomEdge")
+        else if (this.gameObject.name == "BottomEdge")
             _destination = GameObject.Find("TopEdge");
         else
             Debug.LogWarning("bad name for the edges");
     }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.name == "UFO")
-        {
-            Teleport(other.gameObject);
-        }
+        Teleport(other.gameObject);
     }
 
     void Teleport(GameObject gameObject)
     {
         TeleportationMargin = gameObject.GetComponent<CircleCollider2D>().bounds.size.magnitude;
-        if(this.gameObject.name == "LeftEdge" || this.gameObject.name == "RightEdge")
+        if (this.gameObject.name == "LeftEdge" || this.gameObject.name == "RightEdge")
         {
             float newX = _destination.transform.position.x;
             newX += (this.gameObject.name == "LeftEdge") ? -TeleportationMargin : TeleportationMargin;
             gameObject.transform.position = new Vector3(newX, gameObject.transform.position.y, gameObject.transform.position.z);
         }
-        else if(this.gameObject.name == "TopEdge" || this.gameObject.name == "BottomEdge")
+        else if (this.gameObject.name == "TopEdge" || this.gameObject.name == "BottomEdge")
         {
             float newY = _destination.transform.position.y;
             newY += (this.gameObject.name == "TopEdge") ? TeleportationMargin : -TeleportationMargin;
