2ee03b827a76c37eb49d0330b0816a900940e22c
Fabian Achammer <fabian.achammer@gmail.com>
2016-04-09 23:40:01 +0200

    fixed leaked animation and sound

--- Assets/Scripts/GameOverHandler.cs
+++ Assets/Scripts/GameOverHandler.cs
@@ -2,18 +2,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 using UnityEngine.Assertions;
-using System.Collections;
 
 public class GameOverHandler : MonoBehaviour
 {
-
     public GameplayManager gameplayManager;
 
     public GameOverScreen gameOverScreen;
 
     public GameRestartController gameRestartController;
-
-    private Queue<IEnumerator> coroutinesToExecute = new Queue<IEnumerator>();
 
     private void Awake()
     {
@@ -22,24 +18,6 @@ public class GameOverHandler : MonoBehaviour
         Assert.IsNotNull(gameRestartController);
         gameRestartController.enabled = false;
         gameplayManager.PlayerWins += OnPlayerWins;
-        gameplayManager.PlayerLoses += OnPlayerLoses;
-    }
-
-    private void Update()
-    {
-        if (coroutinesToExecute.Any())
-        {
-            StartCoroutine(ExecuteCoroutines());
-        }
-    }
-
-    private IEnumerator ExecuteCoroutines()
-    {
-        while (coroutinesToExecute.Any())
-        {
-            var coroutine = coroutinesToExecute.Dequeue();
-            yield return StartCoroutine(coroutine);
-        }
     }
 
     private void OnPlayerWins(Player winningPlayer)
@@ -53,17 +31,6 @@ public class GameOverHandler : MonoBehaviour
             component.enabled = false;
         }
 
-        coroutinesToExecute.Enqueue(WinningPlayerRoutine(winningPlayer));
-    }
-
-    private IEnumerator WinningPlayerRoutine(Player winningPlayer)
-    {
-        yield return StartCoroutine(gameOverScreen.SetWinningPlayer(winningPlayer));
         gameRestartController.enabled = true;
-    }
-
-    private void OnPlayerLoses(Player losingPlayer)
-    {
-        coroutinesToExecute.Enqueue(gameOverScreen.SetLeakedPlayer(losingPlayer));
     }
 }
