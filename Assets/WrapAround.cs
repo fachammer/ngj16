31038a229fc45118abbc8e94729aea1bda284f69
Roman Luks <rluks@seznam.cz>
2016-04-09 14:22:56 +0200

    left and right wrap around better

--- Assets/WrapAround.cs
+++ Assets/WrapAround.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class WrapAround : MonoBehaviour {
 
+    public float TeleportationMargin = 3.0f;
+
     private GameObject _destination;
+
 
 	// Use this for initialization
 	void Start () {
-        _destination = transform.GetChild(0).gameObject;
-	}
+        if(this.gameObject.name == "LeftEdge")
+            _destination = GameObject.Find("RightEdge");
+        else if(this.gameObject.name == "RightEdge")
+            _destination = GameObject.Find("LeftEdge");
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -25,8 +31,17 @@ public class WrapAround : MonoBehaviour {
 
     void Teleport(GameObject gameObject)
     {
+        print(this.gameObject.name);
+        TeleportationMargin = gameObject.GetComponent<CircleCollider2D>().bounds.size.magnitude;
+        print("tm"+TeleportationMargin);
         if(this.gameObject.name == "LeftEdge" || this.gameObject.name == "RightEdge")
-            gameObject.transform.position = new Vector3(_destination.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        {
+            float newX = _destination.transform.position.x;
+            print("ö"+newX);
+            newX += (this.gameObject.name == "LeftEdge") ? -TeleportationMargin : TeleportationMargin;
+            print("n"+newX);
+            gameObject.transform.position = new Vector3(newX, gameObject.transform.position.y, gameObject.transform.position.z);
+        }
         else if(this.gameObject.name == "TopEdge" || this.gameObject.name == "BottomEdge")
             gameObject.transform.position = new Vector3(gameObject.transform.position.x, _destination.transform.position.y, gameObject.transform.position.z);
         else
