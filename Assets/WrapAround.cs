78c42970b9f97b30fd5657a7c0c5536a6a5d5343
Roman Luks <rluks@seznam.cz>
2016-04-09 14:28:58 +0200

    wrap around top and bottom unfinished

--- Assets/WrapAround.cs
+++ Assets/WrapAround.cs
@@ -3,17 +3,23 @@ using System.Collections;
 
 public class WrapAround : MonoBehaviour {
 
-    public float TeleportationMargin = 3.0f;
+    public float TeleportationMargin;
 
     private GameObject _destination;
 
-
 	// Use this for initialization
 	void Start () {
+
         if(this.gameObject.name == "LeftEdge")
             _destination = GameObject.Find("RightEdge");
         else if(this.gameObject.name == "RightEdge")
             _destination = GameObject.Find("LeftEdge");
+        else if(this.gameObject.name == "TopEdge")
+            _destination = GameObject.Find("BottomEdge");
+        else if(this.gameObject.name == "BottomEdge")
+            _destination = GameObject.Find("TopEdge");
+        else
+            Debug.LogWarning("bad name for the edges");
     }
 	
 	// Update is called once per frame
@@ -31,15 +37,11 @@ public class WrapAround : MonoBehaviour {
 
     void Teleport(GameObject gameObject)
     {
-        print(this.gameObject.name);
         TeleportationMargin = gameObject.GetComponent<CircleCollider2D>().bounds.size.magnitude;
-        print("tm"+TeleportationMargin);
         if(this.gameObject.name == "LeftEdge" || this.gameObject.name == "RightEdge")
         {
-            float newX = _destination.transform.position.x;
-            print("ö"+newX);
+            float newX = _destination.transform.position.x;;
             newX += (this.gameObject.name == "LeftEdge") ? -TeleportationMargin : TeleportationMargin;
-            print("n"+newX);
             gameObject.transform.position = new Vector3(newX, gameObject.transform.position.y, gameObject.transform.position.z);
         }
         else if(this.gameObject.name == "TopEdge" || this.gameObject.name == "BottomEdge")
