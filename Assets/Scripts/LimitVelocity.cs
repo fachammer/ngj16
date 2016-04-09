cef8383d2078f36698c1b020762758de4a9c8936
Roman Luks <rluks@seznam.cz>
2016-04-09 18:16:56 +0200

    limit velocity as separate script

---
+++ Assets/Scripts/LimitVelocity.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LimitVelocity : MonoBehaviour {
+
+    public float MaximumSpeed = 5.0f;
+    public bool LimitingSpeed;//flag whether we currently limit the speed or not
+
+    private Rigidbody2D _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        //limit velocity
+        float speed = Vector3.Magnitude (_rigidBody.velocity);
+        if(speed > MaximumSpeed)
+        {
+            LimitingSpeed = true;
+            _rigidBody.velocity = Vector3.ClampMagnitude(_rigidBody.velocity, MaximumSpeed);
+        }
+        else
+        {
+            LimitingSpeed = false;
+        }
+
+    }
+}
