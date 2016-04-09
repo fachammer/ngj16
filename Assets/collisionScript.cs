fea218be94ca53d78db4e889a0d9c59124d23611
Henrik Esmann <esmann.henrik@gmail.com>
2016-04-10 01:33:25 +0200

    skybox, satellites rotates, spawns objects as radar beeps, decreases beep opacity over time

---
+++ Assets/collisionScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class collisionScript : MonoBehaviour {
+
+    public GameObject satellite;
+
+    public RandomMover rndm;
+    public satellite_beep_spawner spwn;
+    public rotationScript rotScript;
+
+	// Use this for initialization
+	void Start () {
+
+        /*
+        satellite = transform.parent.gameObject.transform.Find("satelliteParent").transform.Find("satellite Mesh").gameObject;
+        rndm = satellite.GetComponent<RandomMover>();
+        spwn = satellite.GetComponent<satellite_beep_spawner>();
+        */
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "player") {
+            
+        }
+    }
+}
---
+++ Assets/satellite_beep_spawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class satellite_beep_spawner : MonoBehaviour {
+
+    public GameObject beep;
+    private float currentTime;
+    public float spawnDelay;
+    private GameObject followObject;
+    private GameObject bleep;
+    public bool chasing;
+
+	// Use this for initialization
+	void Start () {
+        chasing = false;
+        currentTime = Time.time;
+        followObject = transform.parent.gameObject.transform.Find("Satellite Mesh").transform.Find("followObject").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.position = followObject.transform.position;
+
+        if (chasing == false)
+        {
+            if (currentTime + spawnDelay <= Time.time)
+            {
+                //GameObject bleep;
+                bleep = Instantiate(beep, transform.position, Quaternion.identity) as GameObject;
+
+                currentTime = Time.time;
+            }
+        }
+
+        if (bleep) { 
+        bleep.transform.position = followObject.transform.position;
+    }
+    }
+
+}
