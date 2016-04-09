5b83f45f01cb55b593a4db2329fa3f1557066b04
Roman Luks <rluks@seznam.cz>
2016-04-09 14:54:18 +0200

    limiting velocity

--- Assets/Scripts/Controlls.cs
+++ Assets/Scripts/Controlls.cs
@@ -7,6 +7,8 @@ public class Controlls : MonoBehaviour
     public PlayerNo PlayerNumber = PlayerNo.ONE;
 
     public float Speed = 100.0f;
+    public float MaximumSpeed = 5.0f;
+    public bool LimitingSpeed;//flag whether we currently limit the speed or not
     public float ThrustIncrement = 0.01f;
     public Vector2 ThrustForce;
 
@@ -71,12 +73,27 @@ public class Controlls : MonoBehaviour
 
     void FixedUpdate()
     {
-        //AddRelativeForce()
-        //apply force in direction relative to the object
-        //so force applied in vector up is up in the coordinates
-        //of the object
         _rigidBody.AddRelativeForce(_thrustForce);
-        //_rigidBody.AddForce();
+
+        //limit velocity
+        float speed = Vector3.Magnitude (_rigidBody.velocity);
+        if(speed > MaximumSpeed)
+        {
+            //print("S"+speed+" ms:"+MaximumSpeed);
+            LimitingSpeed = true;
+            //float brakeSpeed = speed - MaximumSpeed;  // calculate the speed decrease
+
+            //Vector3 normalisedVelocity = _rigidBody.velocity.normalized;
+            //Vector3 brakeVelocity = normalisedVelocity * brakeSpeed;  // make the brake Vector3 value
+
+            _rigidBody.velocity = Vector3.ClampMagnitude(_rigidBody.velocity, MaximumSpeed);
+
+            //_rigidBody.AddForce(-brakeVelocity);  // apply opposing brake force
+        }
+        else
+        {
+            LimitingSpeed = false;
+        }
 
     }
 }
